e340bf40e7425970247ce49e9dcd850aed7e5fdb
vsynychych <vsynychych@gmail.com>
2020-12-11 01:22:38 +0200

    Lesson2

--- src/components/allPosts/AllPosts.js
+++ src/components/allPosts/AllPosts.js
@@ -3,23 +3,31 @@ import Post from "../post/Post";
 
 class AllPosts extends Component {
 
+    state = {posts: [], chosenOne: null}
 
-    state={posts:[]}
     componentDidMount() {
         fetch("http://jsonplaceholder.typicode.com/posts")
             .then(value => value.json())
-            .then(value =>{
-                this.setState({posts:value})
+            .then(value => {
+                this.setState({posts: value});
             })
     }
 
+    onPostChose = (id) => {
+        let chosenOne = this.state.posts.find(value => value.id === id);
+        this.setState({chosenOne});
+    }
+
+
     render() {
-        let{posts} =this.state
+        let {posts, chosenOne} = this.state
+        let cls = "green";
         return (
             <div>
-                {posts.map((value)=>(<Post  item={value} />))}
+                {posts.map((value) => (<Post item={value} key={value.id} onPostChose={this.onPostChose}/>))}
+                {chosenOne && <h2 className={cls}>{chosenOne.id}-{chosenOne.title} </h2>}
             </div>
-        );
+        )
     }
 }
 
--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
-
-
+import './Post.css'
 
 class Post extends Component {
 
     render() {
-        console.log(this.props);
-        let {item}=this.props
+        // console.log(this.props.item);
+        let {item, onPostChose} = this.props
+        let color = "beige";
+        let button = "btn";
         return (
-            <div>
+            <div className={color}>
+                <button className={button} onClick={() => onPostChose(item.id)}>chose</button>
                 {item.id} - {item.title}
             </div>
         );
